8ffdeca7024f54fd6ced381db76899538f953506
Will Roden <roden011@gmail.com>
2019-06-15 23:02:15 -0400

    A lot of progress on the prequestionnaire

--- crywolf/crywolf.py
+++ crywolf/crywolf.py
@@ -5,8 +5,20 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html', title = "Title passed from View to Template",
+    return render_template('index.html', section = "Section title passed from View to Template",
                             text = "Text passed from View to Template")
+
+@app.route('/prequestionnaire')
+def prequestionnaire():
+    return render_template('prequestionnaire.html')
+
+@app.route('/experiment')
+def experiment():
+    return render_template('experiment.html')
+
+@app.route('/postsurvey')
+def postsurvey():
+    return render_template('postsurvey.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
