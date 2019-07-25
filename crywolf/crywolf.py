2783738a4d1d0b6821cd5ee511d8c220eae30942
Will Roden <roden011@gmail.com>
2019-07-25 12:46:17 -0400

    Prequestionnaire db progress

--- crywolf/crywolf.py
+++ crywolf/crywolf.py
@@ -10,6 +10,9 @@ app.config['SECRET_KEY'] = '+xjtZo+YaAWKhCSky9nLCubHvPCjhRRxN45niWNVaN4='
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'crywolf.db')
 db = SQLAlchemy(app)
 
+from forms import PrequestionnaireForm
+import models
+
 @app.route('/index')
 @app.route('/')
 def index():
@@ -18,8 +21,46 @@ def index():
 
 @app.route('/prequestionnaire', methods = ["GET", "POST"])
 def prequestionnaire():
-    #if request.method== "POST":
-    #    return redirect(url_for('index'))
+    form = PrequestionnaireForm()
+    if form.validate_on_submit():
+        one = form.one.data
+        twoA = form.twoA.data
+        twoB = form.twoB.data
+        twoC = form.twoC.data
+        twoD = form.twoD.data
+        threeA = form.threeA.data
+        threeB = form.threeB.data
+        threeC = form.threeC.data
+        threeD = form.threeD.data
+        threeE = form.threeE.data
+        four = form.four.data
+        five = form.five.data
+        six = form.six.data
+        seven = form.seven.data
+        eight = form.eight.data
+        nine = form.nine.data
+        ten = form.ten.data
+        answers = models.PrequestionnaireAnswers(
+            one = one,
+            twoA = twoA,
+            twoB = twoB,
+            twoC = twoC,
+            twoD = twoD,
+            threeA = threeA,
+            threeB = threeB,
+            threeC = threeC,
+            threeD = threeD,
+            threeE = threeE,
+            four = four,
+            five = five,
+            six = six,
+            seven = seven,
+            eight = eight,
+            nine = nine,
+            ten = ten)
+        db.session.add(answers)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('prequestionnaire.html')
 
 @app.route('/experiment')
